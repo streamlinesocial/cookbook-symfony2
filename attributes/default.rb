c03743ccb57d8c9a3f19dd8697302499dd37f24a
Chris <chris@buryta.com>
2013-03-22 00:37:19 -0400

    added tweaks to params to enable more control over the parameters.yml config file in the attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,26 @@
 # centos 6 with epel-release repo tested
-default["symfony"]["packages"] = [ 'wget', 'libjpeg', 'libpng', 'giflib', "php", "php-devel", "php-cli", "php-pear", "php-xml", "php-xmlrpc", "php-mbstring", "php-mysql", "php-pdo", "php-pecl-apc", "php-gd" ]
+default["symfony"]["packages"] = [ 'wget', 'libjpeg', 'libpng', 'giflib',
+                                   "php", "php-devel", "php-cli", "php-pear", "php-xml",
+                                   "php-xmlrpc", "php-mbstring", "php-mysql", "php-pdo",
+                                   "php-pecl-apc", "php-gd" ]
 
 default['symfony']['server_name'] = 'symfony-app.dev'
 default['symfony']['server_aliases'] = ['www.symfony-app.dev']
 default['symfony']['server_docroot'] = "/var/www/vhosts/#{default['symfony']['server_name']}/current/public/web"
 
-default['symfony']['mysql_user'] = 'root'
-default['symfony']['mysql_pass'] = ''
-default['symfony']['mysql_host'] = 'localhost'
-default['symfony']['mysql_name'] = 'symfony_dev'
+# used to construct the yml file
+default['symfony']['parameters']['database_driver']     = "pdo_mysql"
+default['symfony']['parameters']['database_host']       = "localhost"
+default['symfony']['parameters']['database_port']       = "~"
+default['symfony']['parameters']['database_name']       = "symfony_dev"
+default['symfony']['parameters']['database_user']       = "root"
+default['symfony']['parameters']['database_password']   = ""
+default['symfony']['parameters']['mailer_transport']    = "smtp"
+default['symfony']['parameters']['mailer_host']         = "localhost"
+default['symfony']['parameters']['mailer_user']         = "~"
+default['symfony']['parameters']['mailer_password']     = "~"
+default['symfony']['parameters']['locale']              = "en"
+default['symfony']['parameters']['secret']              = "ThisTokenIsNotSoSecretChangeIt"
 
 default["symfony"]["repository"] = 'https://github.com/streamlinesocial/app-symfony.git'
 default["symfony"]["revision"] = 'master'
--- recipes/deploy_vagrant.rb
+++ recipes/deploy_vagrant.rb
@@ -1,16 +1,6 @@
 release_path = "/var/www/vhosts/#{node['symfony']['server_name']}"
 
-deployUser = node["symfony"]["deploy_user"]
-deployGroup = node["symfony"]["deploy_group"]
-deployRepo = node["symfony"]["repository"]
-deployBranch = node["symfony"]["revision"]
-
-environmentVars = ({ 'MYSQL_DB'     => node["symfony"]["mysql_name"],
-                     'MYSQL_USER'   => node["symfony"]["mysql_user"],
-                     'MYSQL_PASS'   => node["symfony"]["mysql_pass"],
-                     'MYSQL_HOST'   => node["symfony"]["mysql_host"],
-                     'APACHE_USER'  => node["apache"]["user"],
-                     'APACHE_GROUP' => node["apache"]["group"] })
+environmentVars = ()
 
 # setup db connection and other app settings
 template "#{release_path}/public/app/config/parameters.yml" do
@@ -19,36 +9,12 @@ template "#{release_path}/public/app/config/parameters.yml" do
     # owner node['symfony']['deploy_user']
     # group node['symfony']['deploy_group']
     mode "644"
-    variables(
-        :mysql_user => node['symfony']['mysql_user'],
-        :mysql_pass => node['symfony']['mysql_pass'],
-        :mysql_host => node['symfony']['mysql_host'],
-        :mysql_name => node['symfony']['mysql_name']
-    )
+    variables()
 end
 
 # before_migrate
 # setup vendors and ensure install
-execute "script/deploy/before_migrate.sh" do
+execute "ant deploy" do
     cwd release_path
     environment environmentVars
-    # user deployUser
-    # group deployGroup
-end
-
-# migration_command
-# runs after symlinks are created
-execute "script/deploy/migration.sh" do
-    environment environmentVars
-    cwd release_path
-    # user deployUser
-    # group deployGroup
-end
-
-# runs after migration
-execute "script/deploy/before_restart.sh" do
-    environment environmentVars
-    cwd release_path
-    # user deployUser
-    # group deployGroup
 end
